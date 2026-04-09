bd595d3febd737cf229c55a9fed81d9269c9d38f
livinusonynso <livinusonyenso@gmail.com>
2026-04-09 22:14:54 +0100

    fix cores error

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ const app = express();
 // ─── Global Middleware ────────────────────────────────────────────────────────
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:8081",
     optionsSuccessStatus: 200,
   })
 );
